ec619f582e5263fd7f0d1ecb4e234b60f139e770
Nikita Alekseyonok <ext-nikita-alekseyonok@rencore.com>
2024-03-28 12:11:19 +0300

    Update Logger class

--- TestTask/helpers/logger_helper.py
+++ TestTask/helpers/logger_helper.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any
 
 from loguru import logger
 
@@ -9,26 +10,25 @@ class Logger:
 
     __logger.add(sys.stderr, format="<blue>[{level}]</blue> : <green>{message}</green>", colorize=True)
 
-
     @staticmethod
-    def info(message):
+    def info(message: str) -> None:
         Logger.__logger.info(message)
 
     @staticmethod
-    def debug(message):
+    def debug(message: str) -> None:
         Logger.__logger.debug(message)
 
     @staticmethod
-    def warning(message):
+    def warning(message: str) -> None:
         Logger.__logger.warning(message)
 
     @staticmethod
-    def error(message):
+    def error(message: str) -> None:
         Logger.__logger.error(message)
 
     @staticmethod
-    def step(message: str):
-        def step_decorator(step_method):
+    def step(message: str) -> Any:
+        def step_decorator(step_method: Any) -> Any:
             Logger.__logger.info(f'STEP: {message}')
             return step_method
 
